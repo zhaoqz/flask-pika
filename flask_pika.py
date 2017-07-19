86bf88a8a848f60c4ba4207fb8f907efd94f58c9
jinghuiace <jinghuiace@126.com>
2017-07-19 19:46:54 +0800

    fix xrange 2 range

--- flask_pika.py
+++ flask_pika.py
@@ -38,7 +38,7 @@ class Pika(object):
         if pool_params is not None:
             self.pool_size = pool_params['pool_size']
             self.pool_recycle = pool_params['pool_recycle']
-            for i in xrange(self.pool_size):
+            for i in range(self.pool_size):
                 channel = PrePopulationChannel()
                 self.__set_recycle_for_channel(channel, -1)
                 self.pool_queue.put(channel)
